13be7df1df86aed17a918facb461700b3a3449df
Carlos Garcia <carlosgm128@gmail.com>
2021-07-09 11:51:16 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 //Install express server
 const express = require("express");
 const path = require("path");
-
+const morgan = require("morgan");
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
 // Serve only the static files form the dist directory
+app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(`./dist/angular-deploy`));
-app.get(`/*`, function(req, res) {
-    res.sendFile(`index.html`, {root: `dist/angular-deploy`}
-  );
-  });
+
+app.get(`/*`, function (req, res) {
+  res.sendFile(`index.html`, { root: `dist/angular-deploy` });
+});
 
 // app.get("/", (req, res) => {
 //   //console.log(`server runing at server port ${app.get("port")}`);
